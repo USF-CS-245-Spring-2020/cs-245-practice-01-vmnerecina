57240c773ac0bab8e4ac1e1f88129f3829940171
vmnerecina <49412243+vmnerecina@users.noreply.github.com>
2020-01-30 19:51:38 -0800

    Update Practice01MathIterative.java

--- Practice01MathIterative.java
+++ Practice01MathIterative.java
@@ -7,19 +7,6 @@ contains public int fib (int n)
 
 */
 
-/**
- * Practice 01 Math
- * CS 245 - Part of the first practice assignment
- * This is the interface for the classes you must implement.
- * @author dbrizan
- *
- */
-public interface Practice01Math 
-{
-    int fact(int n) throws Exception;
-    int fib(int n) throws Exception;
-}
-
 
 class Practice01MathIterative implements Practice01Math
 {
